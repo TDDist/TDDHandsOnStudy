db0fb31b2acc4b77bb20e87c8352a2212af8b001
ehgud0670 <kimdo2297@gmail.com>
2021-04-25 15:05:13 +0900

    [refactoring] 인수가 nil 포함가능하도록 변경

--- 6~7강_테스트_주도_개발/ehoud0670/Lecture6And7/Lecture6And7/TextRefiner.swift
+++ 6~7강_테스트_주도_개발/ehoud0670/Lecture6And7/Lecture6And7/TextRefiner.swift
@@ -10,19 +10,19 @@ import Foundation
 class TextRefiner {
     func execute(args: String, bannedWords: [String]? = nil) -> String {
         var source = args
+        
         source = source.normalizedWhiteSpaces()
         source = source.compactedWhiteSpaces()
         source = source.trimmingCharacters(in: [" "])
-        if let bannedWords = bannedWords {
-            source = source.mask(bannedWords: bannedWords)
-        }
+        source = source.mask(bannedWords: bannedWords)
         
         return source
     }
 }
 
 private extension String {
-    mutating func mask(bannedWords: [String]) -> String {
+    mutating func mask(bannedWords: [String]?) -> String {
+        guard let bannedWords = bannedWords else { return self }
         return bannedWords.reduce(self) { $0.replacingOccurrences(of: $1, with: String.init(repeating: "*", count: $1.count)) }
     }
     
