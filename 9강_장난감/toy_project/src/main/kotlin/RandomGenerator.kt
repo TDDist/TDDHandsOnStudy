c87de7140394c73d2a10160d5ba68b53b12d8808
ehgud0670 <kimdo2297@gmail.com>
2021-05-09 14:52:59 +0900

    [update] 중복된 프로젝트 제거

--- 9강_장난감/toy_project/src/main/kotlin/RandomGenerator.kt
+++
@@ -1,9 +0,0 @@
-import kotlin.random.Random
-
-class RandomGenerator : PositiveIntegerGenerator {
-    private val random = Random(0)
-
-    override fun generateLessThanOrEqualToHundred(): Int {
-        return random.nextInt(100) + 1
-    }
-}
--- 9강_장난감/toy_project/src/test/java/PositiveIntegerGeneratorStub.kt
+++
@@ -1,7 +0,0 @@
-class PositiveIntegerGeneratorStub(vararg val numbers : Int) : PositiveIntegerGenerator {
-    private var index = 0
-    override fun generateLessThanOrEqualToHundred(): Int {
-        index = index++ % numbers.size
-        return numbers[index]
-    }
-}
