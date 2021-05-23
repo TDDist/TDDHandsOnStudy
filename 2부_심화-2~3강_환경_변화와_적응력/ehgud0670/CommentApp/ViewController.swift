516a3596d9576239b9b1e53c599c19d9fd5786eb
ehgud0670 <kimdo2297@gmail.com>
2021-05-23 20:52:50 +0900

    [refactoring] ViewModel 생성

--- 2부_심화-2~3강_환경_변화와_적응력/ehgud0670/CommentApp/ViewController.swift
+++ 2부_심화-2~3강_환경_변화와_적응력/ehgud0670/CommentApp/ViewController.swift
@@ -13,14 +13,10 @@ class ViewController: UIViewController {
     @IBOutlet weak var contentTextField: UITextField!
     @IBOutlet weak var contentStack: UIStackView!
     
-    //MARK:- Properties
-    private var contentRefiner: ContentRefinable!
-    private let timeStamper = TimeStamper()
+    private let commentViewModel = CommentViewModel(contentRefiner: ContentRefiner())
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        self.contentRefiner = ContentRefiner()
     }
     
     @IBAction func inputButtonDidTouch(_ sender: UIButton) {
@@ -31,12 +27,11 @@ class ViewController: UIViewController {
         guard let author = author, let content = content else { return }
         guard !author.isEmpty, !content.isEmpty else { return }
         
-        let comment = Comment(
-            content: contentRefiner.execute(content: content, bannedWords: nil),
-            author: author,
-            time: timeStamper.makeTimeDescription()
-        )
+        // update view model
+        commentViewModel.updateComments(author: author, content: content)
         
+        // create CommentView
+        guard let comment = commentViewModel.latestComment else { return }
         let commentView = CommentView(comment: comment)
         self.contentStack.addArrangedSubview(commentView)
     }
---
+++ 2부_심화-2~3강_환경_변화와_적응력/ehgud0670/CommentApp/ViewModel/CommentViewModel.swift
@@ -0,0 +1,33 @@
+//
+//  CommentViewModel.swift
+//  CommentApp
+//
+//  Created by 스마트스터디_김도형 on 2021/05/23.
+//
+
+import Foundation
+
+final class CommentViewModel {
+    private var contentRefiner: ContentRefinable!
+    private let timeStamper: TimeStamper
+    private var comments = [Comment]()
+    
+    init(contentRefiner: ContentRefinable, timeStamper: TimeStamper = TimeStamper()) {
+        self.contentRefiner = contentRefiner
+        self.timeStamper = timeStamper
+    }
+    
+    func updateComments(author: String, content: String) {
+        let comment = Comment(
+            content: contentRefiner.execute(content: content, bannedWords: nil),
+            author: author,
+            time: timeStamper.makeTimeDescription()
+        )
+        
+        comments.append(comment)
+    }
+    
+    var latestComment: Comment? {
+        return comments.last
+    }
+}
